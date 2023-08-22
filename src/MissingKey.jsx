4e203d651f5d4246e4e4d8b16b269a24ddd81228
Sean Fagan <faganseanp@gmail.com>
2023-08-22 08:47:31 -0600

    committing initial code for clone

---
+++ src/MissingKey.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function MissingKey() {
+  const [emojis, setEmojis] = useState([
+    {id: 1, emoji: "😁"},
+    {id: 2, emoji: "😘"},
+    {id: 3, emoji: "🤪"},
+    {id: 4, emoji: "🤗"}
+  ]);
+  const deleteEmoji = emojiId => {
+    const updatedEmojis = emojis.filter(emoji => emoji.id !== emojiId);
+    setEmojis(updatedEmojis);
+  };
+
+  return (
+    <div>
+      <h2>Rate these emojis!</h2>
+      {emojis.map((emoji) => (
+        <li className="emoji-item">
+          {emoji.emoji}
+          <select>
+            <option>Bad</option>
+            <option>Okay</option>
+            <option>Very good</option>
+          </select>
+          <button onClick={() => deleteEmoji(emoji.id)}>Delete</button>
+        </li>
+      ))}
+    </div>
+  );
+}
